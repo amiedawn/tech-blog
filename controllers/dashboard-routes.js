f68a336e63827260477f8a14168cb5c468bd90aa
amiedawn <amie.d.carroll@gmail.com>
2021-01-17 18:18:43 -0700

    new post working

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -95,8 +95,36 @@ router.get("/edit/:id", withAuth, (req, res) => {
     });
 });
 
-router.get("/new", (req, res) => {
-  res.render("add-post");
+router.get("/create/", withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "created_at", "post_content"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("add-post", { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
