96bdb5df7d638fe13b322d29ffe493ba166869dc
amiedawn <amie.d.carroll@gmail.com>
2021-01-18 10:47:26 -0700

    revise readme

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,7 +2,8 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const post_content = document.querySelector('input[name="post-content"]').value;
+  const post_content = document.querySelector('input[name="post-content"]')
+    .value;
 
   const response = await fetch(`/api/posts`, {
     method: "POST",
@@ -20,8 +21,10 @@ async function newFormHandler(event) {
   } else {
     alert(response.statusText);
   }
+ 
 }
 
-document
-  .querySelector(".new-post-form")
-  .addEventListener("submit", newFormHandler);
+
+ document
+   .querySelector(".new-post-form")
+   .addEventListener("submit", newFormHandler);
